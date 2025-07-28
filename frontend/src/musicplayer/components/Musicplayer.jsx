6173ea951288bd880f6ab73e19ab1b2f71a94aa4
George Clapham Holguín <claphamholguingeorge@gmail.com>
2025-07-28 14:20:15 -0500

    changing the visual aspect and html element hierarchy

--- frontend/src/musicplayer/components/Musicplayer.jsx
+++ frontend/src/musicplayer/components/Musicplayer.jsx
@@ -31,35 +31,38 @@ useEffect(() => {
 
 
 return(
-    <div className="container" id='MusicPlayerContainer'>
+    <div className="container" id='MusicPlayerContainer' style={{ maxWidth: '600px'}}>
         <div className="row">
-            <div className="col" id="MusiplayerTitleColumn">
+            <div className="col-8" id="MusiplayerTitleColumn">
                 <h1>Music player</h1>
             </div>
         </div>
-        <div className="row align-items-center">
-            <div className="col-3" id="bandImageColumn">
-                <img src={`http://localhost:8080/${musicData.length > 0 ? currentSong.album.band.imageLocation : img}`} alt="" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+  
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-6" id="bandImageColumn">
+                <img src={`http://localhost:8080/${musicData.length > 0 ? currentSong.album.band.imageLocation : img}`} alt="" style={{ maxWidth: '250px', height: 'auto', objectFit: 'contain' }} />
             </div>
-            <div className="col-6" id="bandInfoColumn">
-                        <h1 id="bandSongName">{musicData.length > 0 ? currentSong.name : <span>Loading...</span>}</h1>
-                        <h4 id="bandName">{musicData.length > 0 ? currentSong.album.band.name : <span>Loading...</span>}</h4>
-                        <h6 id="bandAlbumName">{musicData.length > 0 ? currentSong.album.name : <span>Loading...</span>}</h6>
+            <div className="col-12 col-md-6" id="bandInfoColumn">
+                        <h1 class ="text-body-emphasis" id="bandSongName">{musicData.length > 0 ? currentSong.name : <span>Loading...</span>}</h1>
+                        <h4 class ="text-dark-emphasis" id="bandName">{musicData.length > 0 ? currentSong.album.band.name : <span>Loading...</span>}</h4>
+                        <h6 class ="text-body-secondary" id="bandAlbumName">{musicData.length > 0 ? currentSong.album.name : <span>Loading...</span>}</h6>
             </div>
         </div>
+        <br />
         <div className='row justify-content-center'>
-            <div className='col-3 text-end' id="songTimeColumn">
+            <div className='col-2 text-end' id="songTimeColumn">
                 00:00
             </div>
-            <div className='col-6 text-center' id="songProgressBarColumn">
+            <div className='col-8 text-center' id="songProgressBarColumn">
                 <div className="progress">
                     <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{ width: '75%' }}>75%</div>
                 </div>
             </div>
-            <div className='col-3 text-start' id="songTimeLeftColumn">
+            <div className='col-2 text-start' id="songTimeLeftColumn">
                 00:00
             </div>
         </div>
+
         <div className='row'>
             <div className='col text-center'>
                 ABC
